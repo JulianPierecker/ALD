c11c1fa5dc8ee3df2e6fd1576932b30db2198336
JulianPierecker <72915799+JulianPierecker@users.noreply.github.com>
2023-10-18 11:25:45 +0200

    Initial Commit

---
+++ SLL_TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SingleLinkedList;
+
+namespace SLL_TestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList<int> myList = new SingleLinkedList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+
+            bool exists = myList.Contains(6);
+
+            Console.WriteLine(exists.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SingleLinkedList/SingleLinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    internal class Node<T>
+    {
+        public Node(T item)
+        {
+            m_data = item;
+        }
+        public Node<T> m_next { get; set; }
+        public T m_data { get; set; }
+    }
+    public class SingleLinkedList<T>
+    {
+        Node<T> m_head { get; set; }
+        Node<T> m_last { get; set; }
+        int m_count { get; set; }
+
+        public void Add(T item)
+        {
+            m_count++;
+            Node<T> tmp = new Node<T>(item);
+            if(m_head == null) 
+            {
+                m_head = tmp;
+                m_last = tmp;
+                return;
+            }
+
+            m_last.m_next = tmp;
+            m_last = tmp;
+        }
+
+        public bool Contains(T item)
+        {
+            for (Node<T> i = m_head; i != null; i = i.m_next)
+            {
+                if (i.m_data.Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
